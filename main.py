fe9696199a7a4b919b7cfea1d6095ae14aa6731b
Akshat Khurana <akshat.khurana@DETNSW>
2024-02-29 13:03:09 +1100

    Update main.py
    Testing API.

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-"""
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return render_template('sample.html')
-
-if __name__ == '__main__':
-    app.run()
-"""
-
 from dotenv import load_dotenv
 import requests, sys, os, json
+import pprint
 
 # API key
-API_KEY = ""
+API_KEY = "b249505b7e912e3b3b34821a4533c6f1"
 
+# Input
+city = input('City: ')
 
-# Retrieve
-# requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q=&limit=1&appid={API_KEY}')
+# Retrieve information
+raw_location_data = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}')
+
+location_data = json.loads((raw_location_data.content).decode())
+longitude = location_data[0].get('')
