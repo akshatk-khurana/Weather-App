dcb84c18812ded2b16133afd7b5e4e825b53e2f6
Akshat Khurana <akshat.khurana@DETNSW>
2024-03-01 11:08:49 +1100

    Added more functionality.
    Have to show the user a weather summary and work on dealing with errors; letting them know whether the given input was wrong or server is down.

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from dotenv import load_dotenv
-import requests, sys, os, json
-import pprint
+import requests, sys, os, json, pprint, keyboard
+from datetime import datetime
+from iso3166 import countries
 
-# API key
-API_KEY = "b249505b7e912e3b3b34821a4533c6f1"
+API_KEY = "b249505b7e912e3b3b34821a4533c6f1" # API key
 
-# Input
-city = input('City: ')
+# Define needed functions
+def get_weather(city):
+    raw_weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}')
+    weather_data = (raw_weather_data.json())
+    city_name, country_name = weather_data['name'], countries.get(weather_data['sys']['country']).name
+    print(f"Showing current weather in {city_name}, {country_name}")
+    return city_name, country_name
 
-# Retrieve information
-raw_location_data = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}')
+# Main while loop to keep prompting user
+first_time = True; default = "Press [I] for help, [W] to get weather for a city, [H] for history and [Q] to quit: "
+while True:
+    choice = input("Welcome to CMD Weather! " + default if first_time == True else default).strip().lower()
 
-location_data = json.loads((raw_location_data.content).decode())
-longitude = location_data[0].get('')
+    if first_time == True:
+        first_time = False
+
+    if choice == 'i':
+        print('You pressed help!')
+    elif choice == 'w':
+        city = input('What city would you like to view weather for?: ').strip()
+        try:
+            get_weather(city)
+        except:
+            print('An error, occurred!')
+    elif choice == 'h':
+        print('Showing history!')
+    elif choice == 'q':
+        print('Exiting application!')
+        sys.exit()
+    else:
+        print('Unknown command entered.')
