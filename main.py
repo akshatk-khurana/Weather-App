ced215ea8971f6c86b00acc845b8316fa9763ca5
Akshat Khurana <akshat.khurana@yahoo.com>
2024-03-05 09:06:03 +1100

    Updated history

--- main.py
+++ main.py
@@ -1,63 +1,93 @@
-import requests, sys, os, json, pprint   
+import requests, sys, os, json, time
 from datetime import datetime
 from iso3166 import countries
 
 API_KEY = "b249505b7e912e3b3b34821a4533c6f1" # API key
 
 # Define needed functions
-def readable_time(unix):
+def readable_time(unix): 
     return datetime.fromtimestamp(int(unix)).strftime('%Y-%m-%d %H:%M:%S')
 
-def get_weather(city):
+def get_weather(city): # Fetch and return data from API
     raw_weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric')
     status = raw_weather_data.status_code
 
+    # Status code error handling
     if 500 <= status <= 599: print('The server was down. Try again in some time.')
     elif 400 <= status <= 499: print('Incorrect city. Check the name of city you typed in.')
     else: return sort_data(raw_weather_data.json())
     return None
 
 def sort_data(data):
-    pprint.pprint(data)
+    # Define dictionary to contain sorted data
     info = dict()
-    info['place'] = f"{data['name']}, {countries.get(data['sys']['country']).name}, "
+
+    # Add selected data to above defined dictionary
+    info['place'] = f"{data['name']}, {countries.get(data['sys']['country']).name}"
     info['humidity'] = data['main']['humidity']
     info['temp'], info['feels_like'] = data['main']['temp'], data['main']['feels_like']
     info['max'], info['min'] = data['main']['temp_max'], data['main']['temp_min']
     info['weather'] = data['weather'][0]['description']
-    info['sunrise'], info['sunset'] = readable_time['sys']['sunrise'], readable_time['sys']['sunset']
+    info['sunrise'], info['sunset'] = readable_time(data['sys']['sunrise']), readable_time(data['sys']['sunset'])
+    
     return info
 
-def load_history(number):
+def load_history(n):
     with open("history.json", "r") as history:
         selected = json.loads(history.read())
-        return {i:selected[i] for i in list(selected.keys())[0:number]}
 
-def save_history(item):
-    pass
+        # Return the last n searches and results
+        return {i:selected[i] for i in list(selected.keys())[::-1][0:int(n)]}
+
+def save_history(item) -> None:
+    with open('history.json', 'r') as read_history:
+        # Set key value pair of timestamp and what was looked up at that time
+        history = json.loads(read_history.read())
+        history[datetime.now().strftime('%Y-%m-%d %H:%M:%S')] = item
+
+        # Update the JSON file
+        with open('history.json', 'w') as write_history:
+            write_history.write(json.dumps(history, indent=4))
 
 def clear_history():
-    with open('history.json', 'w') as history: history.write('')
+    # Clear the file and replace with an empty JSON object
+    with open('history.json', 'w') as write_history:
+        print('sdfsd')
+        write_history.write(json.dumps('{}'))
 
 def display(data):
-    print(f"Showing current weather in {data['place']}.")
+    # Display data to the user in a readable format
+    print(f"\nShowing current weather in {data['place']}")
     print(f"Current temperature: {data['temp']} Feels like {data['feels_like']}")
 
 # Main while loop to keep prompting user
 first_time, default = True, "Press [I] for help, [W] to get weather for a city, [H] for history and [Q] to quit: "
 while True:
+    os.system('clear')
     choice = input("Welcome to CMD Weather! " + default if first_time == True else default).strip().lower()
     first_time = False
 
+    # CLI implementation - give user options to choose from
     if choice == 'i':
         print('This is a simple application to show the weather.\nCommands are [W] to get the weather, [H] for history and [Q] to quit.')
     elif choice == 'w':
         city = input('What city would you like to view weather for?: ').strip()
-        display(get_weather(city))
+        data = get_weather(city)
+        display(data); save_history(data)
     elif choice == 'h':
-        number = input("How many items of your session history would you like to view?: ")
-        print('\n'.join(load_history(number)))
-        
-    elif choice == 'q': print('Exiting application!'); sys.exit()
+        number = input("Number of previous searches to view: ")
+        session_history = load_history(number)
+
+        print()
+        for k in session_history.keys():
+            print(f'****{k}****')
+            display(session_history[k]); print()
+    elif choice == 'q': 
+        print('Exiting application!')
+        clear_history()
+        sys.exit()
     else:
         print('Unknown command entered.')
+
+    if choice.lower() in ['i', 'w', 'h']:
+        time.sleep(5)
