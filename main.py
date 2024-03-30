fe843c1f43b14975c73481460ebd6322327859a5
Akshat Khurana <akshat.khurana@yahoo.com>
2024-03-30 18:49:52 +1100

    Final changes
    Make some final changes to the code and comments. Ready for submission.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime # Work with date time
 from iso3166 import countries # To convert country codes to names
 from colorama import Fore, init # To add colours to CLI
 
-API_KEY = "b249505b7e912e3b3b34821a4533c6f1" # API key
+API_KEY = "" # API key
 init() # Initialise colours
 
 # Define needed functions
@@ -16,27 +16,29 @@ def clear_screen(): # Clear screen based off OS
 def get_weather(city): # Fetch and return data from API
     raw_weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric')
     status = raw_weather_data.status_code
-
     # Status code error handling
+    
     if 500 <= status <= 599: # If server errors occur.
-        print('The server was down. Try again in some time.'); return None
+        print('The server was down. Try again in some time.')
     elif 400 <= status <= 499: # If the user inputs invalid city.
-        print('\nIncorrect city. Check the name of city you typed in.\n'); return None
+        if status == 401: # If no API key given
+            print('\nCheck your API key!')
+        else:
+            print('\nIncorrect city. Check the name of city you typed in.\n')
     else: 
         return sort_data(raw_weather_data.json())
+    return None
 
 def degrees_to_compass(degrees): # Convert degrees to compass directions
     directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
-    index = int((degrees + 11.25)/22.5)
-    return directions[index % 16]
+    index = int((degrees + 11.25)/22.5) # Calculate direction
+    return directions[index % 16] # Access appropriate direction from directions list
 
-def sort_data(data): # Sort out needed values
-    info = dict() # Define dictionary to contain sorted data
-    country = None
+def sort_data(data): # Sort out needed values from API data
+    info, country = dict(), None 
     try:
         country = ', '+countries.get(data['sys']['country']).name
-    except:
-        country = ''
+    except: country = '' # If iso3166 cannot find country of the location typed in.
 
     # Add selected data to above defined dictionary
     info['place'] = f"{data['name']}{country}"
@@ -50,12 +52,11 @@ def sort_data(data): # Sort out needed values
 
 def load_history():
     with open("history.json", "r") as history:
-        # Return the last n searches and results
-        return json.loads(history.read())
+        return json.loads(history.read()) # Return past interactions
 
 def save_history(item):
     with open('history.json', 'r') as read_history:
-        # Set key value pair of timestamp and search
+        # Set key value pair of timestamp and interaction
         history = json.loads(read_history.read())
         history[datetime.now().strftime('%H:%M:%S')] = item
 
@@ -70,11 +71,12 @@ def clear_history():
 
 def display(data):
     # Display data to the user in a readable format
-    print(f"{Fore.BLUE}Showing current weather in {data['place']}")
-    print(f"The current temperature is {data['temp']}°C and it feels like {data['feels_like']}°C")
-    print(f"\nHumidity right now is {data['humidity']}%")
+    print(f"{Fore.BLUE}Showing current weather in {data['place']}:\n")
+    print(f"The current temperature is {data['temp']}°C and it feels like {data['feels_like']}°C.")
+    print(f"The maximum temperature for today is {data['max']}°C and the minimum temperature is {data['min']}°C.")
+    print(f"\nHumidity right now is {data['humidity']}%.")
     print(f"Wind direction is {data['wind_direction']} and wind speed is {data['wind_speed']}km/h.\n")
-    print(f"General description of weather today: {data['weather']}")
+    print(f"The general description of weather today is {data['weather']}.")
     print(f"Sunrise time is {data['sunrise']} and sunset time is {data['sunset']}.")
 
 # Main while loop to keep prompting user
@@ -91,17 +93,20 @@ while True:
         print("[Q] will exit the application and clear your search history for the session.\n")
     elif choice == 'w':
         city = input('\nWhat city would you like to view weather for?: ').strip()
-        data = get_weather(city)
-        if data is not None:
-            print(); display(data); print()
-            save_history(data)
+        try:
+            data = get_weather(city)
+            if data is not None:
+                print(); display(data); print()
+                save_history(data)
+        except requests.exceptions.ConnectionError:
+            print('\nCheck your internet connection!')
     elif choice == 'h':
         clear_screen()
         session_history = load_history()
         
         # Allow user to scroll and view history
-        keys = list(session_history.keys())
-        if len(keys) > 0:
+        keys = list(session_history.keys()) # Get a list of timestamps to go back and forth through
+        if len(keys) > 0: # Check if there is any history
             index = 1
             while True:
                 # Show first portion of history
@@ -110,11 +115,11 @@ while True:
                 print('----------------------\n')
 
                 print(f'SHOWING SEARCH {index} OF {len(keys)}')
-                scroll = input('Press [F] to go forwards, [B] to go backwards and [E] to exit history scrolling: ').lower()
+                scroll = input('Press [F] to go forward, [B] to go back and [E] to exit scrolling: ').lower()
                 clear_screen()
-                if scroll == 'b': # Move backwards
+                if scroll == 'b': # Move backwards in history
                     index = index - 1 if index > 1 else index
-                elif scroll == 'f': # Move forwards
+                elif scroll == 'f': # Move forwards in history
                     index = index + 1 if index < len(keys) else index
                 elif scroll == 'e':
                     break # Go back to main loop
@@ -126,5 +131,5 @@ while True:
         print('Unknown command entered.')
 
     # Ask user if they want to clear their screen
-    if input(Fore.RED+"Clear past interactions in the terminal? (y/press any other key): ").lower() == 'y':
+    if input(Fore.RED+"\nClear past interactions in the terminal? (y/press any other key): ").lower() == 'y':
         clear_screen()
